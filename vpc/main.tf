103b91ad456b8c47c76362ccd1c9fe6873926c73
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-13 04:59:02 -0600

    added policy

--- vpc/main.tf
+++ vpc/main.tf
@@ -45,3 +45,30 @@ resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+resource "aws_s3_bucket_policy" "mycompliantpolicy" {
+  bucket = "mycompliantbucketname"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "mycompliantpolicy"
+    Statement = [
+      {
+        Sid       = "HTTPSOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.mycompliantbucket.arn,
+          "${aws_s3_bucket.mycompliantbucket.arn}/*",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
