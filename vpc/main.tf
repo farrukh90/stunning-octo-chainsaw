afaa777daab4075db77084ca6fbfece10985f268
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-13 05:24:47 -0600

    s

--- vpc/main.tf
+++ vpc/main.tf
@@ -75,7 +75,12 @@ resource "aws_s3_bucket" "bucket1" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "my-unique-bucket-name-2"
-  acl    = "private"
+
+resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
