9bfc1c7b923f9c5e6367b6e8aa20baeef105788c
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-12 19:09:08 -0600

    added new tf code

--- vpc/main.tf
+++ vpc/main.tf
@@ -32,3 +32,16 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+
+resource "aws_s3_bucket" "example" {  # Sensitive
+  bucket = "examplename"
+}
+
+resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false # should be true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
