03eb65fef24ca3fc1ab41fd4216d76ef8c5a747b
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-13 04:56:31 -0600

    removed s3

--- vpc/main.tf
+++ vpc/main.tf
@@ -33,15 +33,3 @@ resource "aws_security_group" "allow_tls" {
   }
   
 }
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-
-resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
-  bucket = aws_s3_bucket.example.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
