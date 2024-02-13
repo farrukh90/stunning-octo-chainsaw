50a599b8d79102ea7d2e092630b8a5ca200c08dc
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-13 05:11:42 -0600

    removed s3

--- vpc/main.tf
+++ vpc/main.tf
@@ -33,21 +33,7 @@ resource "aws_security_group" "allow_tls" {
   }
   
 }
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
+
 resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
   bucket = aws_s3_bucket.example.id
 
