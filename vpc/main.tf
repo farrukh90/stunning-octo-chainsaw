29765ea6f174c3e451ceff72b888a095a9e963fd
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-13 05:00:21 -0600

    example

--- vpc/main.tf
+++ vpc/main.tf
@@ -60,8 +60,8 @@ resource "aws_s3_bucket_policy" "mycompliantpolicy" {
         Principal = "*"
         Action    = "s3:*"
         Resource = [
-          aws_s3_bucket.mycompliantbucket.arn,
-          "${aws_s3_bucket.mycompliantbucket.arn}/*",
+          aws_s3_bucket.example.arn,
+          "${aws_s3_bucket.example.arn}/*",
         ]
         Condition = {
           Bool = {
