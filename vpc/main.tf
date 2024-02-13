bd071b0a0a8ce517aa4b51c50bc109dcc4825f22
Farrukh Sadykov <farrukhsadykov@gmail.com>
2024-02-13 05:15:08 -0600

    updated url

--- vpc/main.tf
+++ vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["127.0.0.1/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
 
@@ -32,40 +32,4 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
   
-}
-
-resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
-  bucket = aws_s3_bucket.example.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-
-resource "aws_s3_bucket_policy" "mycompliantpolicy" {
-  bucket = "mycompliantbucketname"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "mycompliantpolicy"
-    Statement = [
-      {
-        Sid       = "HTTPSOnly"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource = [
-          aws_s3_bucket.example.arn,
-          "${aws_s3_bucket.example.arn}/*",
-        ]
-        Condition = {
-          Bool = {
-            "aws:SecureTransport" = "false"
-          }
-        }
-      },
-    ]
-  })
 }
